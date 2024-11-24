7a1b61afd2dbbcaa9e9f6b7fc8f2ac372a32f21f
moradiyaparth27 <56012306+moradiyaparth27@users.noreply.github.com>
2024-11-24 10:42:58 +0000

    Delete hello_world.py

--- hello_world.py
+++
@@ -1,3 +0,0 @@
-print("Hello World!")
-print('print("Hello World!")')      # this line prints-> print("Hello World!")
-print('\"Python is my \"FAVOURITE\" language and I get "Excited!" while coding in it.\"')       # this line prints-> "Python is my "FAVOURITE" language and I get 'Excited!' doing coding in it."
