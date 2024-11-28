1fbad401e058fa8e71018089a9a9fb62b0af07fe
moradiyaparth27 <moradiyaparth3@gmail.com>
2024-11-28 12:22:07 +0000

    Update

--- input_name_demo.py
+++ input_name_demo.py
@@ -1,3 +1,10 @@
 a = input("What's your name? ")
 print("Hello", a + " Hope your'e doing well!")
 print("Hi", a + " I can confirm that your age is: " + input("What's your Age?"))
+
+
+#This line will count the characters with spaces
+print("The total characters in your name counting the spaces in between is: " + str( len (a) ))
+
+#This line will count the characters without spaces in between string
+print("The total characters in your name without counting the spaces in between is: " + str( len (a.replace(' ', '') ) ))
---
+++ name.py
@@ -0,0 +1,2 @@
+a = input("What's your name?")
+print("The total characters in your name without counting the spaces in between is: " + str( len (a.replace (' ', '')) ))
