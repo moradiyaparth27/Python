ac73da75466f307c003373a467b91e62155ed9e7
moradiyaparth27 <moradiyaparth3@gmail.com>
2024-11-28 10:04:14 +0000

    hi

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,5 @@
-print(" Hello World!")
-print(" \"Python\" is my favourite language and I 'LOVE' to do coding in it. ")
+print("Hello World!")
+print("\"Python\" is my favourite language and I 'LOVE' to do coding in it.")
+print("Hi there! \nHi There! \nHi There")
+print("Hello" + "Parth")
+print("Hello" + " " + "Parth")
