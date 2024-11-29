8837f9a7f5af0d9eae1bcbe592a4f3e1115a65eb
moradiyaparth27 <moradiyaparth3@gmail.com>
2024-11-29 14:11:39 +0000

    datatype demo

---
+++ bmi_calculator.py
@@ -0,0 +1,16 @@
+height = input("Enter your height in m: ")
+weight = input("Enter your weight in KG: ")
+
+# One way is to do type conversion by declaring new variables
+new_height = float(height)
+new_weight = float(weight)
+bmi = new_weight/(new_height * new_height)
+new_bmi = int(bmi)
+print(new_bmi)
+
+# Second method is to convert data types directly 
+# Also using the power function by ** method
+bmi = float(weight)/(float(height)*float(height))
+bmi2 = float(weight)/(float(height)**2)
+print(int(bmi))
+print(int(bmi2))
---
+++ data_type_demo.py
@@ -0,0 +1,11 @@
+number = input("Type a two digit number: ")
+first_digit = number[0]
+secod_digit = number[1]
+#converting the str datatype to int during the sum.
+print(int(first_digit) + int(secod_digit))
+
+
+#can convert the datatype str to int before the sum operation as well.
+first_digit = int(number[0])
+secod_digit = int(number[1])
+print(first_digit + secod_digit)
