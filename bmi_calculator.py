0c8de37a6c42dd1613345e6245dcf648ad3136dd
moradiyaparth27 <moradiyaparth3@gmail.com>
2024-11-29 14:59:01 +0000

    Total five methods to convert datatypes from int to float or whatever

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -9,9 +9,16 @@ new_bmi = int(bmi)
 print(new_bmi)
 
 # Second method is to convert datatypes directly 
-# Also using the power function by ** method
 bmi = float(weight)/(float(height)*float(height))
-bmi2 = float(weight)/(float(height)**2)
 print(int(bmi))
+
+# Third using the power function by ** method
+bmi2 = float(weight)/(float(height)**2)
 print(int(bmi2))
+
+# Fourth method is to use // operator to convert float values to integer values directly
+bmi3 = float(weight) // float(height) ** 2
+print(int(bmi3))
+
+# Fifth method is to use round function to decide how many digits should be there in float
 print(round(bmi, 5))
