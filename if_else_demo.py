de1945fc31ea230bdc88b332f9440b1fb2e0b077
moradiyaparth27 <moradiyaparth3@gmail.com>
2024-11-30 13:01:04 +0000

    if else using elif

--- if_else_demo.py
+++ if_else_demo.py
@@ -1,7 +1,7 @@
 print("Welcome to the roller coster ride:")
 height = int(input("Enter your height in cm:"))
 
-if height > 170 :
+if height >= 170 :
     print("You're good to ride.")
 else : 
     print("You're not tall enough")
---
+++ if_else_not_equal_to_operator.py
@@ -0,0 +1,9 @@
+print("Welcome to the roller coster ride:")
+height = input("Are you taller then 170cm? Type 'YES' or 'NO'. ")
+
+if height == "YES" :
+    print("You're good to ride.")
+elif height == "NO" : 
+    print("You're not tall enough")
+else : 
+    print("Enter 'YES' or 'NO' only.")
