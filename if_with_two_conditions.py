23a471fe3ec75b217dafcc0872c2ba5c1e7eb988
moradiyaparth27 <moradiyaparth3@gmail.com>
2024-11-30 13:32:03 +0000

    if else using two elif statements

--- if_with_two_conditions.py
+++ if_with_two_conditions.py
@@ -2,9 +2,11 @@ print("Welcome to the roller coster ride:")
 height = int(input("Enter your height in cm:"))
 age = int(input("Enter your age:"))
 
-if height >= 170 and age <= 18:
+if height >= 170 and age < 12:
+    print("You're good to ride. Please buy $5 ticket.")
+elif height >= 170 and age >= 12 and age <= 18:
     print("You're good to ride. Please buy $7 ticket.")
 elif height >= 170 and age > 18:
     print("You're good to ride. Please buy $12 ticket.")
 else : 
-    print("You're not tall enough")
+    print("You're not tall enough so you can't purchase any ticket.")
