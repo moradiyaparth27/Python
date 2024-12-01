0b0a6215f4838818380e3c58065df68e3de20aa9
moradiyaparth27 <moradiyaparth3@gmail.com>
2024-12-01 13:24:04 +0000

    head or tails against computer

--- heads_tails_using_random_nuber.py
+++ heads_tails_using_random_nuber.py
@@ -1,9 +1,7 @@
 import random
 
-
 random_number = random.randint(0, 1)
 user_input = int(input("Choose 0 for Heads or 1 for Tails: "))
-
 
 if user_input >= 0 and user_input <= 1:
     if user_input == random_number:
