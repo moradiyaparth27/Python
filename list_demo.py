0bfe20a6512d98f7f3c6d3044b6dfaace6679ec8
moradiyaparth27 <moradiyaparth3@gmail.com>
2024-12-01 13:43:27 +0000

    cities in gujarat list demo

---
+++ list_demo.py
@@ -0,0 +1,9 @@
+cities_in_gujarat = ["Surat", "Ahmedabad", "Kachh", "Bhavanagar", "Navsari", "Bardoli"]
+
+# Get output from a list based on their position 
+print(cities_in_gujarat[0])
+print(cities_in_gujarat[1])
+
+# Get output from a list based on their position where -1 is the last position
+print(cities_in_gujarat[-1])
+print(cities_in_gujarat[-2])
