2372c191d7c7bd32b27138ef596190d9b1e28b42
moradiyaparth27 <moradiyaparth3@gmail.com>
2024-12-01 13:03:49 +0000

    random number integer & float

--- random_number_demo.py
+++ random_number_demo.py
@@ -1,4 +1,13 @@
 import random
 
+# Generate random integer number between a and b
 random_integer = random.randint(1, 10)
 print(random_integer)
+
+# Generate random floating number between 0 to 1 but not including
+random_float = random.random()
+print(random_float)
+
+# Generate random floating number between a and b
+random_floating_point = random.uniform(0, 10)
+print(random_floating_point)
