deea5a99dcb72222634fd64a5f5d2574c5fcfa06
moradiyaparth27 <moradiyaparth3@gmail.com>
2024-12-02 08:53:55 +0000

    buzz bee using if condition and modulo operator

---
+++ if_else_buzz.py
@@ -0,0 +1,9 @@
+user_input = int(input("Enter a number: "))
+
+if user_input%5 == 0 :
+    if user_input%3 == 0:
+        print("BuzzBee")
+    else :
+        print("Buzz")
+elif user_input%3 ==0 :
+    print("Bee")
