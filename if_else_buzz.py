497ccec792295da7b269467f94393965fccb5547
moradiyaparth27 <moradiyaparth3@gmail.com>
2024-12-02 11:50:20 +0000

    BuzzBee using if else

--- if_else_buzz.py
+++ if_else_buzz.py
@@ -1,7 +1,7 @@
 user_input = int(input("Enter a number: "))
 
-if user_input%5 == 0 :
-    if user_input%3 == 0:
+if user_input % 5 == 0 :
+    if user_input % 3 == 0:
         print("BuzzBee")
     else :
         print("Buzz")
