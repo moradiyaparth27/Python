8fb8522e54ce1098729341aafa24742daa360a22
moradiyaparth27 <moradiyaparth3@gmail.com>
2024-12-03 11:41:11 +0000

    random.choice

--- password_generator_using_for_loop.py
+++ password_generator_using_for_loop.py
@@ -1,15 +1,23 @@
 import random
 
-letters = int(input("How many letters would you like in your password?"))
-symbol = int(input("How many symbols would you like?"))
-umber = int(input("How many numbers would you like?"))
+max_letters = int(input("How many letters would you like in your password?"))
+max_symbols = int(input("How many symbols would you like?"))
+max_numbers = int(input("How many numbers would you like?"))
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-final_password = []
+password_list2 = []
+for i in range(max_letters):
+    password_list2.append(random.choice(letters))
 
-for password in final_password :
-    final_password = random.choice(letters)
-    print(final_password)
+for i in range(max_numbers):
+    password_list2.append(random.choice(numbers))
+
+for i in range(max_symbols):
+    password_list2.append(random.choice(symbols))
+
+random.shuffle(password_list2)
+password = ''.join(password_list2)
+print(password)
